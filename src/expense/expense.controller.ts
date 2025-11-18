739de671fff7e0be74574e99da0ad242ea7d8905
Ritom-Chak <ritom31052000@gmail.com>
2025-11-18 21:28:43 +0530

    Developed controllers

---
+++ src/expense/expense.controller.ts
@@ -0,0 +1,77 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Delete,
+    Query,
+    ValidationPipe,
+    Put,
+} from '@nestjs/common';
+import {ExpenseService} from "./expense.service";
+import {
+    CreateExpenseDto,
+    DeleteExpenseDto,
+    Expense,
+    GetExpenseDto,
+    GetExpensesDto,
+    UpdateExpenseDto,
+} from "../libs/common/src"
+import {JwtPayload} from "jsonwebtoken";
+import {GetUser} from "../libs/common/src/decorator";
+import { PinoLogger } from 'nestjs-pino';
+
+@Controller('expenses')
+export class ExpenseController {
+    constructor(
+        private readonly expenseService: ExpenseService,
+        private readonly logger: PinoLogger,
+    ) {
+        this.logger.setContext(ExpenseController.name);
+    }
+
+    @Get()
+    async getExpenses(
+        @Query(new ValidationPipe({ transform: true }))
+        getExpensesDto: GetExpensesDto,
+    ): Promise<Expense[]> {
+        this.logger.info('Fetching the expenses.');
+        return this.expenseService.getExpenses(getExpensesDto);
+    }
+
+    @Get('/one')
+    async getExpenseById(
+        @Query(new ValidationPipe({ transform: true }))
+        getExpenseDto: GetExpenseDto,
+    ): Promise<Expense> {
+        this.logger.info('Fetching the expense by id.');
+        return this.expenseService.getExpenseById(getExpenseDto);
+    }
+
+    @Post()
+    async createExpense(
+        @Body() createExpenseDto: CreateExpenseDto,
+        @GetUser() user: JwtPayload,
+    ): Promise<Expense> {
+        this.logger.info('Creating the expense.');
+        return this.expenseService.createExpense(createExpenseDto, user);
+    }
+
+    @Put()
+    async updateExpense(
+        @Body() updateExpenseDto: UpdateExpenseDto,
+        @GetUser() user: JwtPayload,
+    ): Promise<Expense> {
+        this.logger.info('Updating the expense.');
+        return this.expenseService.updateExpense(updateExpenseDto, user);
+    }
+
+    @Delete()
+    async deleteExpense(
+        @Query(new ValidationPipe({ transform: true }))
+        deleteExpenseDto: DeleteExpenseDto,
+    ): Promise<void> {
+        this.logger.info('Deleting the expense.');
+        return this.expenseService.deleteExpense(deleteExpenseDto);
+    }
+}
--- src/expense/expense.module.ts
+++ src/expense/expense.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-//import { AuthModule } from '../auth/auth.module';
-//import { KpiController } from './kpi.controller';
-import {Expense} from "../libs/common/src/entity";
-//import { KpiService } from './kpi.service';
-//import { kpiProviders } from './kpi.providers';
+import {Expense} from "../libs/common/src";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Expense])],
-    //controllers: [KpiController],
-    //providers: [KpiService, ...kpiProviders],
 })
 export class ExpenseModule {}
